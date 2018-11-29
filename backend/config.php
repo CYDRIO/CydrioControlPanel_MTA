c42afc5c0b994183c029e178fbee107acc7b9638
André López <44867242+venadcli@users.noreply.github.com>
2018-11-29 22:21:09 +0100

    Basic elements

--- backend/config.php
+++ backend/config.php
@@ -6,4 +6,4 @@ $config["database_port"] = 3306;
 $config["database_user"] = "cydrio_cp";
 $config["database_pass"] = "123456";
 
-$config["web_title"] = "Cydrio CP | ";
+$config["web_title"] = "Cydrio CP |";
--- index.php
+++ index.php
@@ -3,3 +3,16 @@
 require_once("backend/require.php");
 require_once("frontend/require.php");
 ?>
+
+
+<html>
+    <head>
+        <title><?php echo ($config["web_title"]); ?></title>
+    </head>
+    
+    
+    
+    <body>
+        
+    </body>
+</html>
