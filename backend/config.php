5d065c17c62c5849abbf37d52183104eff6efedd
André López <44867242+venadCLI@users.noreply.github.com>
2018-11-29 23:25:34 +0100

    Update config.php

--- backend/config.php
+++ backend/config.php
@@ -3,7 +3,7 @@
 $config = [];
 $config["database_host"] = "127.0.0.1";
 $config["database_port"] = 3306;
-$config["database_user"] = "cydrio_cp";
-$config["database_pass"] = "123456";
+$config["database_user"] = "root";
+$config["database_pass"] = "";
 
 $config["web_title"] = "Cydrio CP |";
